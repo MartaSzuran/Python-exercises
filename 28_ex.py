d93b5cc3081c30d61a1ebe12ccd1b7fa342d14cd
Marta <marta.szuran@gmail.com>
2021-10-16 22:57:49 +0200

    Daily_practise

---
+++ 28_ex.py
@@ -0,0 +1,32 @@
+# # Python exercise
+# # source - https://www.w3resource.com/python-exercises/python-basic-exercises.php
+# # Python Basic (Part -I) - Exercises, Practice, Solution
+# # Write a Python program to print all even numbers from a given numbers list in the same order
+# # and stop the printing if any numbers that come after 237 in the sequence. Go to the editor
+# Sample numbers list :
+# numbers = [
+#     386, 462, 47, 418, 907, 344, 236, 375, 823, 566, 597, 978, 328, 615, 953, 345,
+#     399, 162, 758, 219, 918, 237, 412, 566, 826, 248, 866, 950, 626, 949, 687, 217,
+#     815, 67, 104, 58, 512, 24, 892, 894, 767, 553, 81, 379, 843, 831, 445, 742, 717,
+#     958,743, 527
+#     ]
+# # Exercise 28
+
+numbers = [
+     386, 462, 47, 418, 907, 344, 236, 375, 823, 566, 597, 978, 328, 615, 953, 345,
+     399, 162, 758, 219, 918, 237, 412, 566, 826, 248, 866, 950, 626, 949, 687, 217,
+     815, 67, 104, 58, 512, 24, 892, 894, 767, 553, 81, 379, 843, 831, 445, 742, 717,
+     958, 743, 527
+     ]
+
+
+def even_numbers(list_of_numbers):
+    for number in list_of_numbers:
+        if number % 2 == 0:
+            print(number)
+        elif number == 237:
+            print(number)
+            break
+
+
+even_numbers(numbers)
