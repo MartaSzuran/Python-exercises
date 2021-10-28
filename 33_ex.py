49bffe442f60f4f550cfbe4d0328504ffddc235d
Marta <marta.szuran@gmail.com>
2021-10-28 09:44:34 +0200

    Daily_practise

--- 33_ex.py
+++ 33_ex.py
@@ -1,19 +1,29 @@
 # # Python exercise
 # # source - https://www.w3resource.com/python-exercises/python-basic-exercises.php
 # # Python Basic (Part -I) - Exercises, Practice, Solution
-# # Write a Python program to sum of two given integers. However, if the sum is between 15 to 20 it will return 20.
+# # Write a Python program to sum of three given integers. However, if two values are equal sum will be zero.
 # # Exercise 33
 
-number_1 = int(input("Input first number: "))
-number_2 = int(input("\nInput second number: "))
+print("Input 3 integers: ")
+numbers = []
+for i in range(3):
+    number = int(input("Number:"))
+    numbers.append(number)
 
 
-def operation(numb_1, numb_2):
-    sum_of_two_numbers = numb_1 + numb_2
-    if 15 <= sum_of_two_numbers <= 20:
-        sum_of_two_numbers = 20
-    return sum_of_two_numbers
+def operation(list_of_numbers):
+    if list_of_numbers[0] == list_of_numbers[1]:
+        sum_of_list_numbers = 0
+        return sum_of_list_numbers
+    elif list_of_numbers[1] == list_of_numbers[2]:
+        sum_of_list_numbers = 0
+        return sum_of_list_numbers
+    elif list_of_numbers[0] == list_of_numbers[2]:
+        sum_of_list_numbers = 0
+        return sum_of_list_numbers
+    else:
+        sum_of_list_numbers = list_of_numbers[0] + list_of_numbers[1] + list_of_numbers[2]
+    return sum_of_list_numbers
 
 
-sum_of_given_numbers = operation(number_1, number_2)
-print("\nSum is: ", sum_of_given_numbers)
+print("Sum of number: ", operation(numbers))
--- 34_ex.py
+++ 34_ex.py
@@ -1,9 +1,8 @@
 # # Python exercise
 # # source - https://www.w3resource.com/python-exercises/python-basic-exercises.php
 # # Python Basic (Part -I) - Exercises, Practice, Solution
-# # Write a Python program that will return true if the two given integer values are equal
-# # or their sum or difference is 5.
-# # Exercise 34
+# # Write a Python program to sum of two given integers. However, if the sum is between 15 to 20 it will return 20.
+# # Exercise 33
 
 number_1 = int(input("Input first number: "))
 number_2 = int(input("\nInput second number: "))
@@ -11,15 +10,10 @@ number_2 = int(input("\nInput second number: "))
 
 def operation(numb_1, numb_2):
     sum_of_two_numbers = numb_1 + numb_2
-    difference_of_two_numbers = numb_1 - numb_2
-    if sum_of_two_numbers == 5:
-        return True
-    elif difference_of_two_numbers == 5 or difference_of_two_numbers == -5:
-        return True
-    elif numb_1 == numb_2:
-        return True
-    else:
-        return False
+    if 15 <= sum_of_two_numbers <= 20:
+        sum_of_two_numbers = 20
+    return sum_of_two_numbers
 
 
-print(operation(number_1, number_2))
+sum_of_given_numbers = operation(number_1, number_2)
+print("\nSum is: ", sum_of_given_numbers)
